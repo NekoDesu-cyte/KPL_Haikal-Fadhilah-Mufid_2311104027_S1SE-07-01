f893c2ba64ffa240c94708647555efed141c2631
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-02-17 16:33:53 +0700

    menambahkan Lines baru

--- 01_Pengantar_Praktikum_KPL/Identitas.js
+++ 01_Pengantar_Praktikum_KPL/Identitas.js
@@ -6,8 +6,23 @@ const input = redline.createInterface({
     output : process.stdout
 })
 
-input.question("Siapa nama kamu?" , (nama) => {
+input.question("Siapa nama kamu? " , (nama) => {
     console.info(`Hello ${nama}`)
+    input.question("Apa jurusan kamu?", (jurusan) => {
+        console.info(`${jurusan}`)
+        input.question("Di Universitas mana?", (universitas) => {
+            console.info(`di Universitas${universitas}`)
+            input.question("Berapa umurnya?", (umur) => {
+                console.info(`${umur} tahun`)
 
-    input.close()
+                
+              
+                input.close()
+            })
+          
+        })
+      
+    })
 })
+
+
