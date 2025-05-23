916f05721178b3d2047feb4d59699661d62db188
Haikal Fadhilah <haikalfadhilah34@gmail.com>
2025-05-23 14:40:49 +0700

    adding Modul 10 Lib

---
+++ 10_Library/TP_Library_2311104027/Main.js
@@ -0,0 +1,9 @@
+const { AkarPersamaanKuadrat, HasilKuadrat } = require('./AljabarLib');
+
+console.log("=== TEST LIBRARY ALJABAR ===");
+
+let akar = AkarPersamaanKuadrat([1, -3, -10]);
+console.log("Akar persamaan x^2 - 3x - 10 =", akar);
+
+let kuadrat = HasilKuadrat([2, -3]);
+console.log("Hasil kuadrat dari (2x - 3)^2 =", kuadrat);
